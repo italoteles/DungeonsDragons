b0476016cf901fcf36398b3f59cb996b40ed83f4
italoteles <italop.teles@gmail.com>
2023-07-17 13:16:57 -0300

    Class component

--- DungensDragonsApp/src/app/app-routing.module.ts
+++ DungensDragonsApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassesComponent } from './classes/classes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'classes', pathMatch: 'full' },
+  { path: 'classes', component: ClassesComponent },
+  { path: '**', redirectTo: 'classes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
