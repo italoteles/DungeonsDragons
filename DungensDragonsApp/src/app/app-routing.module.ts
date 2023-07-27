123a39517ce2bafdc0a8747985c0b2bacbf0fec5
italoteles <italop.teles@gmail.com>
2023-07-27 08:49:04 -0300

    layout adjustments

--- DungensDragonsApp/src/app/app-routing.module.ts
+++ DungensDragonsApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClassesComponent } from './components/classes/classes.component';
 import { MonstersComponent } from './components/monsters/monsters.component';
+import { RacesComponent } from './components/races/races.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'classes', pathMatch: 'full' },
   { path: 'classes', component: ClassesComponent },
   { path: 'monsters', component: MonstersComponent },
+  { path: 'races', component: RacesComponent },
   { path: '**', redirectTo: 'classes', pathMatch: 'full' },
 ];
 
